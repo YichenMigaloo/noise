f49298d2f103c6f0a4e8ee97ec2aa4a3991b2030
YichenMigaloo <98346342+YichenMigaloo@users.noreply.github.com>
2024-10-27 21:23:52 -0400

    Update trainer.py

--- dassl/engine/trainer.py
+++ dassl/engine/trainer.py
@@ -543,7 +543,7 @@ class SimpleTrainer(TrainerBase):
 
 
     def model_inference(self, input):
-        print('shape of input:', input.shape)
+        #print('shape of input:', input.shape)
         return self.model(input)
         classnames = self.dm.dataset.classnames  # Fetch classnames from dataset manager
         return self.model(input, classnames)  # Pass classnames to the model
@@ -576,7 +576,7 @@ class SimpleTrainer(TrainerBase):
         
         # 将 RGB 图像 (3 通道) 和 noiseprint map + conf (2 通道) 合并为 5 通道输入
         combined_input = torch.cat((input, maps_batch), dim=1)  # 在通道维度拼接，最终形成 5 通道张量
-        print("combined_input shape:",combined_input.shape)
+        #print("combined_input shape:",combined_input.shape)
         label = batch['label'].to(self.device)
 
         return combined_input, label
