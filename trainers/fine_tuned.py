4d0c63c51260949a058fc111b7bfbb75c22487b0
YichenMigaloo <98346342+YichenMigaloo@users.noreply.github.com>
2024-11-05 20:12:56 -0500

    Update fine_tuned.py

--- trainers/fine_tuned.py
+++ trainers/fine_tuned.py
@@ -77,6 +77,31 @@ def load_clip_to_cpu(cfg):
     model = clip.build_model(state_dict or model.state_dict())
 
     return model
+
+def load_vit_without_last_layer(cfg):
+    backbone_name = cfg.MODEL.BACKBONE.NAME
+    url = clip._MODELS[backbone_name]
+    model_path = clip._download(url)
+
+    try:
+        model = torch.jit.load(model_path, map_location = 'cpu').eval()
+        state_dict =None
+        model = torch.jit._unwrap_optional(torch.jit._recursive.wrap_cpp_module(model._c))  # unwrap model
+
+    except RuntimeError:
+        state_dict = torch.load(model_path, map_location="cpu")
+    
+    model = clip.build_model(state_dict or model.state_dict())
+    original_forward = model.visual.forward
+
+    def forward_without_proj(x):
+        x = original_forward(x)
+        if hasattr(model.visual, 'proj'):
+            x = x  
+        return x
+
+    model.visual.forward = forward_without_proj
+    return model
     
 class TextEncoder(nn.Module):
 
@@ -127,7 +152,8 @@ class FineTuned_CLIP(TrainerX):
         classnames = self.dm.dataset.classnames
 
         print(f'Loading CLIP (backbone: {cfg.MODEL.BACKBONE.NAME})')
-        clip_model = load_clip_to_cpu(cfg)
+        #clip_model = load_clip_to_cpu(cfg)
+        clip_model = load_vit_without_last_layer(cfg)
         clip_model.float()
 
         print('Building custom CLIP')
