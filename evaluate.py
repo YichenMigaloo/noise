701053fe06bfa8dd0d18530434d8dfaef1e979a0
YichenMigaloo <98346342+YichenMigaloo@users.noreply.github.com>
2024-11-10 23:30:28 -0500

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -412,9 +412,7 @@ def main(args):
                  'dalle2', 'glide_50_27', 'glide_100_10', 'glide_100_27', 'guided', 'ldm_100', 'ldm_200', 'ldm_200_cfg',
                  'sd_512x512', 'sdxl', 'taming', 'deepfake', 'firefly', 'midjourney_v5', 'dalle3', 'faceswap']
     '''
-    dataset_names = ['biggan','cyclegan','dalle2','dalle3','deepfake','eg3d','faceswap','firefly','gaugan',
-                     'glide_50_27','glide_100_10','glide_100_27','guided','ldm_100','ldm_200','ldm_200_cfg',
-                     'midjourney_v5','progan','sd_512x512','sdxl','stargan','stylegan','stylegan2','stylegan3','taming']
+    dataset_names = ['stylegan3','taming']
     
     
     
