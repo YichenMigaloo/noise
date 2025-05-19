1380953b7a5728679b37bf3a672e1f53c7d091c0
YichenMigaloo <98346342+YichenMigaloo@users.noreply.github.com>
2025-05-18 22:20:49 -0400

    Update transforms.py

--- dassl/data/transforms/transforms.py
+++ dassl/data/transforms/transforms.py
@@ -421,9 +421,9 @@ def _build_transform_test(cfg, choices, target_size, normalize):
 
     print(f"+ {target_size} center crop")
     tfm_test += [CenterCrop(input_size)]
-    
-    print("+ gaussian blur (kernel=23, sigma=1.0)")
-    tfm_test += [GaussianBlur(kernel_size=23, sigma=(1.0, 1.0))]  # 固定模糊程度为1.0
+
+    print("+ gaussian blur (kernel=23, sigma=2.0)")
+    tfm_test += [GaussianBlur(kernel_size=23, sigma=(2.0, 2.0))]  # 固定模糊程度为1.0
     print("+ to torch tensor of range [0, 1]")
     tfm_test += [ToTensor()]
 
