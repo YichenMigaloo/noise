b57449d8561bfa475441a622163a8fdc68e65029
YichenMigaloo <98346342+YichenMigaloo@users.noreply.github.com>
2025-05-18 20:05:19 -0400

    update

--- evaluate.py
+++ evaluate.py
@@ -419,12 +419,12 @@ def main(args):
                      'midjourney_v5','progan','sd_512x512','sdxl','stargan','stylegan','stylegan2','stylegan3','taming']
     '''
 
-    dataset_names = ['cyclegan','dalle2','dalle3','deepfake','eg3d','faceswap','firefly','gaugan',
+    dataset_names = ['cyclegan','dalle3','eg3d','firefly','gaugan',
                      'glide_50_27','glide_100_10','glide_100_27','guided','ldm_100','ldm_200','ldm_200_cfg',
                      'midjourney_v5','progan','sd_512x512','sdxl','stargan','stylegan','stylegan2','stylegan3','taming']
     
     
-    dataset_names = ['sdxl','dalle2','guided','taming']
+    #dataset_names = ['sdxl','dalle2','guided','taming']
 
     #dataset_names = ['progan','faceswap','dalle3','midjourney_v5']
     #dataset_names = ['progan','faceswap','midjourney_v5']
